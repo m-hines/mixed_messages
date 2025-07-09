6f63c2f9acfd4b3364fff57ac821f002d7228132
m-hines <mahines9@gmail.com>
2025-07-09 13:55:48 -0400

    Wrote another random message generator, this time using switch syntax.

---
+++ more_mixed_messages.js
@@ -0,0 +1,65 @@
+
+const messageArray = ['You will meet your soulmate at the local Aldi.', 'Another spoonful, please!', 'Ist hier noch frei?', 'Ich bin auf der Treppe gestolpert.', 'No.', 'Yes.', 'Maybe.', 'Du siehst ein bisschen mude aus.', 'Wo ist mein Handy?', 'Ich kann dir helfen.', 'Don\'t think you can, know you can.', 'Whether you think you can, or think you can\'t, you\'re right.', 'You are the placebo.', 'AMDG', 'All the world\'s a stage.', 'Seek and ye shall find.', 'God is androgyne.', 'April is the cruellest month.', 'Fortuna fortes adiuvat.', 'Ich glaube dass Sie wissen wo mein Handy ist.'];
+const randomNumber = Math.floor(Math.random() * 20);
+switch (randomNumber) {
+    case 0:
+        console.log(messageArray[0]);
+        break;
+    case 1:
+        console.log(messageArray[1]);
+        break;
+    case 2:
+        console.log(messageArray[2]);
+        break;
+    case 3:
+        console.log(messageArray[3]);
+        break;
+    case 4:
+        console.log(messageArray[4]);
+        break;
+    case 5:
+        console.log(messageArray[5]);
+        break;
+    case 6:
+        console.log(messageArray[6]);
+        break;
+    case 7:
+        console.log(messageArray[7]);
+        break;
+    case 8:
+        console.log(messageArray[8]);
+        break;
+    case 9:
+        console.log(messageArray[9]);
+        break;
+    case 10:
+        console.log(messageArray[10]);
+        break;
+    case 11:
+        console.log(messageArray[11]);
+        break;
+    case 12:
+        console.log(messageArray[12]);
+        break;
+    case 13:
+        console.log(messageArray[13]);
+        break;
+    case 14:
+        console.log(messageArray[14]);
+        break;
+    case 15:
+        console.log(messageArray[15]);
+        break;
+    case 16:
+        console.log(messageArray[16]);
+        break;
+    case 17:
+        console.log(messageArray[17]);
+        break;
+    case 18:
+        console.log(messageArray[18]);
+        break;
+    case 19:
+        console.log(messageArray[19]);
+        break;
+}
